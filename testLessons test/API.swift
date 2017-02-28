0af2a73192621a38a41efa03d50eb468271796bc
Mikhail Seregin <mikhailseregin@macbook-pro-mikhail.local>
2017-02-28 17:02:38 +0300

    add alomofire

---
+++ testLessons test/API.swift
@@ -0,0 +1,63 @@
+//
+//  API.swift
+//  testLessons test
+//
+//  Created by Mikhail Seregin on 28.02.17.
+//  Copyright © 2017 Mikhail Seregin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+protocol APIMethodProtocol: URLRequestConvertible {
+  var method: Alamofire.HTTPMethod { get }
+  var path: String { get }
+  var params: Alamofire.Parameters? { get }
+}
+
+
+enum API: APIMethodProtocol {
+ 
+  case getLoans
+  
+  static let baseURLString = "https://api.kivaws.org/v1/"
+  
+  var method: HTTPMethod {
+    switch self {
+    case .getLoans:
+      return .get
+    }
+  }
+  
+  var path: String {
+    switch self {
+    case .getLoans:
+      return API.baseURLString + "loans/newest.json"
+    }
+  }
+  
+  var params: Parameters? {
+    switch self {
+    default:
+      return nil
+    }
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let url = try path.asURL()
+    
+    var urlRequest = Alamofire.URLRequest(url: url)
+    urlRequest.httpMethod = method.rawValue
+    
+    switch method {
+    case .get:
+      return try Alamofire.URLEncoding.default.encode(urlRequest, with: params)
+    case .post:
+      return try Alamofire.JSONEncoding.default.encode(urlRequest, with: params)
+    default:
+      break
+    }
+    return urlRequest
+  }
+}
