b4f4f775cf8f725f8ff4ace3826831d546876565
Mikhail Seregin <mikhailseregin@MacBook-Pro-Mikhail.local>
2017-02-28 15:10:32 +0300

    secondCommit

---
+++ testLessons test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testLessons test
+//
+//  Created by Mikhail Seregin on 28.02.17.
+//  Copyright © 2017 Mikhail Seregin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
